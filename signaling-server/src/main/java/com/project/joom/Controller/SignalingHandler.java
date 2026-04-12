26dbc3cc416abe4f59806bde446e2e0c3614f2ac
Jieun Shin <jieunshin9633@gmail.com>
2026-04-12 16:02:25 +0900

    순단 시 10초 대기후 삭제로 변경 - ScheduledFuture 활용

--- signaling-server/src/main/java/com/project/joom/Controller/SignalingHandler.java
+++ signaling-server/src/main/java/com/project/joom/Controller/SignalingHandler.java
@@ -12,6 +12,9 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Map;
+import java.util.concurrent.*;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
@@ -19,6 +22,10 @@ public class SignalingHandler extends TextWebSocketHandler {
     private final ObjectMapper objectMapper;
     private final RoomService roomService;
     private final WebSocketSessionStore sessionStore;
+    // 삭제할 예약 작업을 관리 ScheduledFuture <- 작업 예약증
+    private final Map<String, ScheduledFuture<?>> removeTasks = new ConcurrentHashMap<>();
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
@@ -34,6 +41,13 @@ public class SignalingHandler extends TextWebSocketHandler {
         String sessionId    = session.getId();
         switch (msg.getType()) {
             case "JOIN":
+                // 만약 이 사용자가 삭제 대기중이었다면 취소 (재연결 성공)
+                if(removeTasks.containsKey(msg.getFrom())) {
+                    // cancel(false): "아직 실행 전이라면 실행하지 마.
+                    removeTasks.get(msg.getFrom()).cancel(false);
+                    removeTasks.remove(msg.getFrom());
+                    log.info("사용자 {} 재연결됨. 삭제 예약 취소.", msg.getFrom());
+                }
                 roomService.join(msg.getRoomId(), msg.getFrom(), sessionId);
                 break;
 
@@ -60,23 +74,29 @@ public class SignalingHandler extends TextWebSocketHandler {
         String roomId    = (String) session.getAttributes().get("roomId");
         String sessionId = session.getId();
 
+        Boolean alreadyLeft = (Boolean) session.getAttributes().getOrDefault("ALREADY_LEFT", false);
+
         log.info("WebSocket disconnected: userId={}, sessionId={}", userId, session.getId());
 
-
-        // 💡 2. 이미 LEAVE 메시지를 통해 나갔는지 확인합니다.
-        Boolean alreadyLeft = (Boolean) session.getAttributes().getOrDefault("ALREADY_LEFT", false);
+        // 사용자가 직접 나가기를 누른 경우는 즉시 정리
         if (alreadyLeft) {
-            log.info("이미 LEAVE 메시지로 처리된 세션입니다. sessionId={}", sessionId);
+            roomService.leave(roomId, userId, sessionId);
             sessionStore.remove(sessionId);
             return; // 중복 실행 방지: 여기서 종료
         }
+        
+        // 순단 시 10초 대기후 삭제
+        if(userId != null && roomId != null) {
+            log.info("사용자 {} 연결 끊김 감지. 10초간 복구 대기 ... ", userId);
+            ScheduledFuture<?> task = scheduler.schedule(()-> {
+                log.info("사용자 {} 유예 시간 만료. 세션 정리 시작.", userId);
+                roomService.leave(roomId, userId, sessionId);
+                removeTasks.remove(userId);
+            }, 10, TimeUnit.SECONDS);
+            removeTasks.put(userId, task);
+        }
         System.out.println("연결 끊김 감지 : "+ sessionId);
         sessionStore.remove(session.getId());
-
-        if(userId != null && roomId != null) {
-            roomService.leave(roomId, userId, sessionId);
-            System.out.println("연결 끊김 감지 : "+ sessionId);
-        }
     }
 
 }
